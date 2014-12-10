aaa445ad7b4e5c4beb3b7bc14c5ba7147cf2ec68
Marta Jolanta Łępicka <marta.lepicka@gmail.com>
2014-12-10 22:43:54 +0100

    Added function to ChoiceBox

--- src/com/wmh/com/wmh/MainWindowController.java
+++ src/com/wmh/com/wmh/MainWindowController.java
@@ -1,20 +1,38 @@
 package com.wmh;
-import javafx.fxml.FXMLLoader;
+import javafx.beans.property.ReadOnlyListWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
+
 
 /**
  * Created by Marta on 2014-12-09.
  */
-public class MainWindowController extends VBox{
-    
-   public MainWindowController(){
+public class MainWindowController extends VBox implements Initializable{
 
+   @FXML ChoiceBox functions;
+   @FXML TextField ratioValue; //radius or learning ratio
+   @FXML Button bStart;
+
+   private ObservableList<String> functionList = FXCollections.emptyObservableList();
+   private ArrayList<String> functionValueList = new ArrayList<String>();
+
+   public MainWindowController(){}
+
+   @Override
+   public void initialize(URL location, ResourceBundle resources) {
+      functionValueList.addAll(Arrays.asList("BackPropagation", "HebbianTraining", "CompetitiveTraining"));
+      functionList = FXCollections.observableArrayList(functionValueList);
+      functions.setItems(functionList);
    }
 }
