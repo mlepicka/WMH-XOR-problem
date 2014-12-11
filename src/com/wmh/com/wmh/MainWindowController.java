7118edaebd4b656747044e6c71289a3deee6e8d4
Marta Jolanta Łępicka <marta.lepicka@gmail.com>
2014-12-11 14:17:09 +0100

    Learning is in the new thread now (start/stop)

--- src/com/wmh/com/wmh/MainWindowController.java
+++ src/com/wmh/com/wmh/MainWindowController.java
@@ -3,6 +3,7 @@ import javafx.beans.property.ReadOnlyListWrapper;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,6 +11,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import org.encog.Encog;
 import org.encog.engine.network.activation.ActivationSigmoid;
 import org.encog.neural.data.NeuralData;
@@ -36,6 +39,8 @@ public class MainWindowController extends VBox implements Initializable{
    @FXML ChoiceBox functions;
    @FXML TextField ratioValue; //radius or learning ratio
    @FXML Button bStart;
+   @FXML Button bStop;
+   @FXML TextFlow textFlow;
    /**
     * The input necessary for XOR.
     */
@@ -49,6 +54,7 @@ public class MainWindowController extends VBox implements Initializable{
 
    private ObservableList<String> functionList = FXCollections.emptyObservableList();
    private ArrayList<String> functionValueList = new ArrayList<String>();
+   private Task currentTask;
 
    public MainWindowController(){}
 
@@ -70,70 +76,120 @@ public class MainWindowController extends VBox implements Initializable{
          // create training data
          // MLDataSet trainingSet = new BasicMLDataSet(XOR_INPUT, XOR_IDEAL);
          NeuralDataSet dataSet = new BasicNeuralDataSet(XOR_INPUT, XOR_IDEAL);
-         int epoch = 1;
-         if(functions.selectionModelProperty().getValue().equals("BackPropagation")){
-//back propagation
-            final Backpropagation backpropagation = new Backpropagation(network, dataSet);
-
-            //ONE
-            do {
-               backpropagation.iteration();
-               System.out.println("Epoch #" + epoch + " Error:" + backpropagation.getError());
-               epoch++;
-            } while (backpropagation.getError() > 0.01);
-            backpropagation.finishTraining();
-
-            // test the neural network
-            System.out.println("Neural Network Results:");
-            for (NeuralDataPair pair : dataSet) {
-               final NeuralData output = network.compute(pair.getInput());
-               System.out.println(pair.getInput().getData(0) + "," + pair.getInput().getData(1)
-                       + ", actual=" + output.getData(0) + ",ideal=" + pair.getIdeal().getData(0));
-            }
+         final int[] epoch = {1};
+         if(functions.getSelectionModel().getSelectedItem().equals("BackPropagation")){
+         //back propagation
             network.reset();
+            final Backpropagation backpropagation = new Backpropagation(network, dataSet);
+            textFlow.getChildren().clear();
+            Thread thread = new Thread(
+               currentTask=new Task() {
+                  @Override
+                  protected Object call ()throws Exception {
+                     //ONE
+                     do {
+                        if (!this.isCancelled()) {
+                           backpropagation.iteration();
+                           System.out.println("Epoch #" + epoch[0] + " Error:" + backpropagation.getError());
+                           epoch[0]++;
+                        }
+                     } while (backpropagation.getError() > 0.01);
+                     backpropagation.finishTraining();
+                     return null;
+                  }
+               });
+
+            thread.setDaemon(true);
+            thread.start();
+
+            currentTask.setOnSucceeded(ev->{
+               // test the neural network
+               System.out.println("Neural Network Results:");
+               for (NeuralDataPair pair : dataSet) {
+                  final NeuralData output = network.compute(pair.getInput());
+                  Text result = new Text("\t"+pair.getInput().getData(0) + "," + pair.getInput().getData(1)
+                          + ",\n  actual=" + output.getData(0) + ",\t ideal=" + pair.getIdeal().getData(0));
+                  textFlow.getChildren().add(result);
+               }
+               network.reset();
+            });
          }
-         else if(functions.selectionModelProperty().getValue().equals("HebbianTraining")){
+         else if(functions.getSelectionModel().getSelectedItem().equals("HebbianTraining")){
             //TWO
             //hebbian training
-            final HebbianTraining hebbianTraining = new HebbianTraining(network, dataSet, false, 0.01);
-            do {
-               hebbianTraining.iteration();
-               System.out.println("Epoch #" + epoch + " Error:" + hebbianTraining.getError());
-               epoch++;
-            } while (hebbianTraining.getError() > 0.01);
-            hebbianTraining.finishTraining();
-
-            // test the neural network
-            System.out.println("Neural Network Results:");
-            for (NeuralDataPair pair : dataSet) {
-               final NeuralData output = network.compute(pair.getInput());
-               System.out.println(pair.getInput().getData(0) + "," + pair.getInput().getData(1)
-                       + ", actual=" + output.getData(0) + ",ideal=" + pair.getIdeal().getData(0));
-            }
+            /**
+             * It's something wrong with hebbianTraining - the error is always 0.00 in 1 iteration which isn't true at all
+             */
             network.reset();
+            final HebbianTraining hebbianTraining = new HebbianTraining(network, dataSet, false, 3.00);
+
+            textFlow.getChildren().clear();
+            Thread thread = new Thread(
+                    currentTask=new Task() {
+                       @Override
+                       protected Object call ()throws Exception {
+                           do {
+                              hebbianTraining.iteration();
+                              System.out.println("Epoch #" + epoch[0] + " Error:" + hebbianTraining.getError());
+                              epoch[0]++;
+                           } while (hebbianTraining.getError() > 0.01);
+                           hebbianTraining.finishTraining();
+                          return null;
+                       }
+                    });
+
+            thread.setDaemon(true);
+            thread.start();
+            // test the neural network
+            currentTask.setOnSucceeded(ev -> {
+               System.out.println("Neural Network Results:");
+               for (NeuralDataPair pair : dataSet) {
+                  final NeuralData output = network.compute(pair.getInput());
+                  Text result = new Text("\t" + pair.getInput().getData(0) + "," + pair.getInput().getData(1)
+                          + ",\n actual=" + output.getData(0) + ",\tideal=" + pair.getIdeal().getData(0));
+                  textFlow.getChildren().add(result);
+               }
+               network.reset();
+            });
          }
-         else {
+         else if(functions.getSelectionModel().getSelectedItem().equals("CompetitiveTraining")) {
             //THREE
             //WTA
             NeighborhoodFunction neighborhoodFunction = new NeighborhoodSingle();
-            final CompetitiveTraining competitiveTraining = new CompetitiveTraining(network,0.01,  dataSet, neighborhoodFunction);
-            do {
-               competitiveTraining.iteration();
-               System.out.println("Epoch #" + epoch + " Error:" + competitiveTraining.getError());
-               epoch++;
-            } while (competitiveTraining.getError() > 0.01);
-            competitiveTraining.finishTraining();
-
-            // test the neural network
-            System.out.println("Neural Network Results:");
-            for (NeuralDataPair pair : dataSet) {
-               final NeuralData output = network.compute(pair.getInput());
-               System.out.println(pair.getInput().getData(0) + "," + pair.getInput().getData(1)
-                       + ", actual=" + output.getData(0) + ",ideal=" + pair.getIdeal().getData(0));
-            }
             network.reset();
+            final CompetitiveTraining competitiveTraining = new CompetitiveTraining(network, 1.0,  dataSet, neighborhoodFunction);
+            textFlow.getChildren().clear();
+            Thread thread = new Thread(
+                    currentTask=new Task() {
+                       @Override
+                       protected Object call ()throws Exception {
+                           do {
+                              competitiveTraining.iteration();
+                              System.out.println("Epoch #" + epoch[0] + " Error:" + competitiveTraining.getError());
+                              epoch[0]++;
+                           } while (competitiveTraining.getError() > 0.01);
+                           competitiveTraining.finishTraining();
+                          return null;
+                       }
+                    });
+
+            thread.setDaemon(true);
+            thread.start();
+            // test the neural network
+            currentTask.setOnSucceeded(ev -> {
+               System.out.println("Neural Network Results:");
+               for (NeuralDataPair pair : dataSet) {
+                  final NeuralData output = network.compute(pair.getInput());
+                  Text result = new Text("\t" + pair.getInput().getData(0) + "," + pair.getInput().getData(1)
+                          + ",\n actual=" + output.getData(0) + ",\tideal=" + pair.getIdeal().getData(0));
+                  textFlow.getChildren().add(result);
+               }
+               network.reset();
+            });
          }
          Encog.getInstance().shutdown();
       });
+
+      bStop.setOnAction(event->{if(currentTask!=null)currentTask.cancel();});
    }
 }
