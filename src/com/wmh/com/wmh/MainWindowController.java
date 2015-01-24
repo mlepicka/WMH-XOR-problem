906149b9900ca30e92f833a52b42504b61481bf2
Marta Jolanta Łępicka <marta.lepicka@gmail.com>
2015-01-24 15:07:53 +0100

    Drawing chart in real time

--- src/com/wmh/com/wmh/MainWindowController.java
+++ src/com/wmh/com/wmh/MainWindowController.java
@@ -22,11 +22,13 @@ import org.encog.neural.data.NeuralDataSet;
 import org.encog.neural.data.basic.BasicNeuralDataSet;
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.structure.NeuralStructure;
 import org.encog.neural.networks.training.competitive.CompetitiveTraining;
 import org.encog.neural.networks.training.competitive.neighborhood.NeighborhoodFunction;
 import org.encog.neural.networks.training.competitive.neighborhood.NeighborhoodSingle;
 import org.encog.neural.networks.training.hebbian.HebbianTraining;
 import org.encog.neural.networks.training.propagation.back.Backpropagation;
+import org.encog.util.simple.EncogUtility;
 import sun.java2d.loops.XORComposite;
 
 import java.net.URL;
@@ -38,7 +40,6 @@ import java.util.*;
  */
 public class MainWindowController extends VBox implements Initializable{
 
-   @FXML ChoiceBox functions;
    @FXML ChoiceBox dimension;
    @FXML ChoiceBox layers;
    @FXML TextField neuronNumbers;
@@ -49,34 +50,32 @@ public class MainWindowController extends VBox implements Initializable{
    @FXML LineChart errorChart;
    @FXML ProgressBar progressBar;
    @FXML Label labelProgress;
+   int iterationNr = 0;
+
 
    private Task currentTask;
-   private ObservableList<XYChart.Series> chartData = FXCollections.emptyObservableList();
-   private ArrayList<XYChart.Series> errorSeries = new ArrayList<>();
-   private ObservableList<XYChart.Data> observableErrorData = FXCollections.emptyObservableList();
-   private ArrayList<XYChart.Data> errorData = new ArrayList<>();
-
+   private XYChart.Series errorSeries = new XYChart.Series();
+   private List<XYChart.Data> errorData = new LinkedList<>();
    public MainWindowController(){}
 
    @Override
    public void initialize(URL location, ResourceBundle resources) {
       //chartProgress.
-      chartData = FXCollections.observableArrayList(errorSeries);
-      Bindings.bindContent(errorSeries, chartData);
-      Bindings.bindContent(errorData, observableErrorData);
+
       errorChart.setCreateSymbols(false);
       BasicNetwork network = new BasicNetwork();
-      functions.setItems(FXCollections.observableArrayList(Arrays.asList("BackPropagation", "HebbianTraining", "CompetitiveTraining")));
+     // functions.setItems(FXCollections.observableArrayList(Arrays.asList("BackPropagation", "HebbianTraining", "CompetitiveTraining")));
       dimension.setItems(FXCollections.observableArrayList(Arrays.asList(2,3,4,5,6,7,8,9)));
       layers.setItems(FXCollections.observableArrayList(Arrays.asList(1,2,3,4)));
-      functions.getSelectionModel().select(0); //default value
+      //functions.getSelectionModel().select(0); //default value
       dimension.getSelectionModel().select(0);
       layers.getSelectionModel().select(0);
       bStart.setOnAction(event -> {
+
+         errorData.clear();
          network.clearContext();
          errorChart.getData().clear();
-         //TODO
-         //dynamic creating of data set for other dimensions
+         errorSeries.getData().clear();
          NeuralDataSet dataSet;
 
          int actualDimension = (int)Math.pow(2,(Integer)dimension.getSelectionModel().getSelectedItem());
@@ -99,15 +98,20 @@ public class MainWindowController extends VBox implements Initializable{
          }
 
          dataSet = new BasicNeuralDataSet(XOR_STATIC, XOR_STATIC_IDEAL);
+         //input layer
          network.addLayer(new BasicLayer(null, true, (Integer)dimension.getSelectionModel().getSelectedItem()));
-         network.addLayer(new BasicLayer(new ActivationSigmoid(), true, (Integer)layers.getSelectionModel().getSelectedItem()));
+         //change for loop and get numbers of neuron from textfield
+         for(int i=0; i< (Integer)layers.getSelectionModel().getSelectedItem(); i++)
+            network.addLayer(new BasicLayer(new ActivationSigmoid(), true, (Integer)layers.getSelectionModel().getSelectedItem()));
+         //output layer
          network.addLayer(new BasicLayer(new ActivationSigmoid(), false, 1));
          network.getStructure().finalizeStructure();
          network.reset();
 
-         final int[] epoch = {1};
-         if (functions.getSelectionModel().getSelectedItem().equals("BackPropagation")) {
-            //back propagation
+         errorSeries.setName("Error");
+         errorChart.getData().add(errorSeries);
+
+
             network.reset();
             final Backpropagation backpropagation = new Backpropagation(network, dataSet);
             textFlow.getChildren().clear();
@@ -118,112 +122,49 @@ public class MainWindowController extends VBox implements Initializable{
                           do {
                              if (!this.isCancelled()) {
                                 backpropagation.iteration();
-                                errorData.add(new XYChart.Data<>(epoch[0],backpropagation.getError()));
-                                //System.out.println("Epoch #" + epoch[0] + " Error:" + backpropagation.getError());
-                                epoch[0]++;
+                                Platform.runLater(()->{
+                                   errorData.add(new XYChart.Data<>(iterationNr,backpropagation.getError()));
+                                   errorSeries.getData().add(new XYChart.Data(iterationNr, backpropagation.getError()));
+                                   iterationNr++;
+                                });
                              }
-                          } while ((backpropagation.getError() > 0.01)&&epoch[0]<100000);
+                          } while ((backpropagation.getError() > 0.01)&&iterationNr<1000);
                           backpropagation.finishTraining();
                           return null;
                        }
                     });
-            thread.setDaemon(true);
+         thread.setDaemon(true);
             thread.start();
-         } else if (functions.getSelectionModel().getSelectedItem().equals("HebbianTraining")) {
-            //hebbian training
-            /**
-             * It's something wrong with hebbianTraining - the error is always 0.00 in 1 iteration which isn't true at all
-             */
-            network.reset();
-            final HebbianTraining hebbianTraining = new HebbianTraining(network, dataSet, false, 3.00);
-            textFlow.getChildren().clear();
-            Thread thread = new Thread(
-                    currentTask = new Task() {
-                       @Override
-                       protected Object call() throws Exception {
-                          do {
-                             hebbianTraining.iteration();
-                             System.out.println("Epoch #" + epoch[0] + " Error:" + hebbianTraining.getError());
-                             errorData.add(new XYChart.Data<>(epoch[0],hebbianTraining.getError()));
-                             epoch[0]++;
-                          } while (hebbianTraining.getError() > 0.01);
-                          hebbianTraining.finishTraining();
-                          return null;
-                       }
-                    });
-            thread.setDaemon(true);
-            thread.start();
-         } else if (functions.getSelectionModel().getSelectedItem().equals("CompetitiveTraining")) {
-            //WTA
-            NeighborhoodFunction neighborhoodFunction = new NeighborhoodSingle();
-            network.reset();
-            final CompetitiveTraining competitiveTraining = new CompetitiveTraining(network, 1.0, dataSet, neighborhoodFunction);
-            textFlow.getChildren().clear();
-            Thread thread = new Thread(
-                    currentTask = new Task() {
-                       @Override
-                       protected Object call() throws Exception {
-                          do {
-                             competitiveTraining.iteration();
-                             System.out.println("Epoch #" + epoch[0] + " Error:" + competitiveTraining.getError());
-                             errorData.add(new XYChart.Data<>(epoch[0], competitiveTraining.getError()));
-                             epoch[0]++;
-                          } while (competitiveTraining.getError() > 0.01);
-                          competitiveTraining.finishTraining();
-                          return null;
-                       }
-                    });
-            thread.setDaemon(true);
-            thread.start();
-         }
 
          final NeuralDataSet finalDataSet = dataSet;
          currentTask.setOnSucceeded(ev -> {
             progressBar.progressProperty().unbind();
+            progressBar.progressProperty().setValue(0.0);
             labelProgress.setText("Network trained.");
             // test the neural network
             for (NeuralDataPair pair : finalDataSet) {
                final NeuralData output = network.compute(pair.getInput());
-               Text result = new Text("\t" + pair.getInput().getData(0) + "," + pair.getInput().getData(1)
-                       + ",\n  actual=" + output.getData(0) + ",\t ideal=" + pair.getIdeal().getData(0));
+               double[] input = pair.getInput().getData();
+               Text result = new Text( "Input array: "+Arrays.toString(input)
+                       + ",\t  actual=" + output.getData(0) + ",\t ideal=" + pair.getIdeal().getData(0)+"\n");
+
                textFlow.getChildren().add(result);
             }
-            Task task;
-            Thread thread1 = new Thread(
-                    task = new Task<Object>() {
-                       @Override
-                       protected Object call() throws Exception {
-                          errorSeries.add(new XYChart.Series("Error", FXCollections.observableArrayList(errorData)));
-                          return null;
-                       }
-                    });
-            task.setOnSucceeded(eve -> {
-               errorChart.setData(FXCollections.observableArrayList(errorSeries));
-               labelProgress.setText("Nothing in progress");
-               progressBar.progressProperty().unbind();
-               progressBar.progressProperty().setValue(0.0);
-            });
-            task.setOnRunning(e -> {
-               //TODO
-               //sth kind of progress bar or drawing chart in real time
-               progressBar.progressProperty().bind(task.progressProperty());
-               labelProgress.setText("Generating chart ...");
-            });
-
-            thread1.setDaemon(true);
-            thread1.start();
-            network.reset();
          });
-         currentTask.setOnRunning(e->{
+         currentTask.setOnRunning(e -> {
             progressBar.progressProperty().bind(currentTask.progressProperty());
             labelProgress.setText("Generating network and training...");
          });
-
+         currentTask.setOnCancelled(e->{
+            labelProgress.setText("Nothing in progress");
+            progressBar.progressProperty().unbind();
+            progressBar.progressProperty().setValue(0.0);
+         });
          Encog.getInstance().shutdown();
       });
 
-      bStop.setOnAction(event -> {
-         if (currentTask != null) currentTask.cancel();
-      });
+         bStop.setOnAction(event -> {
+            if (currentTask != null) currentTask.cancel();
+         });
    }
 }
